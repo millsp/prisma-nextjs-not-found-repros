460c8366eed2a1831d3dc8f24b895834b0f6daec
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2023-02-02 16:44:35 -1000

    add new cases

--- 16_monorepo-serverComponents-customOutput-reExportIndirect/packages/service/next.config.js
+++ 16_monorepo-serverComponents-customOutput-reExportIndirect/packages/service/next.config.js
@@ -1,11 +1,15 @@
+
 const { PrismaPlugin } = require('experimental-prisma-webpack-plugin')
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'standalone',
+  experimental: {
+    appDir: true,
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
-      // config.plugins = [...config.plugins, new PrismaPlugin()]
+      config.plugins = [...config.plugins, new PrismaPlugin()]
     }
 
     return config
